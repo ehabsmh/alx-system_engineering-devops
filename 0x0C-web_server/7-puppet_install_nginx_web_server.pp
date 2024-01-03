acf0b90a7896a4480fb626309a2ffa7310bfce3d
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-03 19:39:17 +0200

    5. Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# Install Nginx web server (w/ Puppet)
+
+package {'nginx':
+	provider => 'apt-get -y',
+}
+
+file {'index.nginx-debian.html':
+	path    => '/var/www/html/index.nginx-debian.html',
+	content => 'Hello World!',
+	ensure  => 'present',
+}
+
+file_line {'redirect':
+	path     => '/etc/nginx/sites-available/default',
+	line     => "\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}",
+	ensure   => 'present',
+	multiple => true,
+	after    => 'error_page 404 /custom_404.html;',
+}
+
+service {'nginx':
+	ensure  => running,
+	require => Package['nginx'],
+}
