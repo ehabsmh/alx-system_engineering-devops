687b24097c6f63c0c70e49bec52b9e3d3de2ea8d
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-29 20:03:14 +0200

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script returns information about his/her TODO list progress."""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    emp_id = argv[1]
+
+    # Get employee by ID
+    emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+        emp_id)
+    ).json()
+
+    # Get all todos by specific employee (by id)
+    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+
+    todos = requests.get(todos_url, params="userId={}".format(emp_id)
+                         ).json()
+
+    # Get completed todos made by specific employee ( by id)
+    completed_todos = requests.get(todos_url, params={"completed": "true",
+                                                      "userId": emp_id}).json()
+
+    emp_name = emp.get("name")
+    total_number_of_tasks = len(todos)
+    number_of_done_tasks = len(completed_todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        emp_name,
+        number_of_done_tasks,
+        total_number_of_tasks
+    ))
+
+    for todo in completed_todos:
+        print(f"\t {todo.get('title')}")
---
+++ 0x15-api/README.md
@@ -0,0 +1,11 @@
+# API
+
+### **This directory contains 4 programs. The programs were written in Python programming language.**
+
+## [0. Gather data from an API](https://github.com/ehabsmh/alx-system_engineering-devops/blob/main/0x15-api/0-gather_data_from_an_API.py)
+
+- Using this [REST API](https://jsonplaceholder.typicode.com/), for a given employee ID, returns information about his/her TODO list progress.
+
+- Acceptc an integer as a parameter, which is the employee ID
+
+---
