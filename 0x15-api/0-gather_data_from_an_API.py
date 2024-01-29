e243a5b7022418af4003756327f9340701442327
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-29 20:03:58 +0200

    Imported libraries are alphabetically ordered

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This script returns information about his/her TODO list progress."""
 
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     emp_id = argv[1]
