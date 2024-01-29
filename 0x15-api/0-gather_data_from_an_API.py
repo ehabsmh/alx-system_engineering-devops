f267afedd81820d5b1445b0e11594383a1a31eab
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-29 20:48:09 +0200

    Refactored

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,32 +5,38 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    emp_id = argv[1]
+    try:
+        emp_id = argv[1]
 
-    # Get employee by ID
-    emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-        emp_id)
-    ).json()
+        # Get employee by ID
+        emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+            emp_id)
+        ).json()
 
-    # Get all todos by specific employee (by id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+        # Get all todos by specific employee (by id)
+        todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
-    todos = requests.get(todos_url, params="userId={}".format(emp_id)
-                         ).json()
+        todos = requests.get(todos_url, params="userId={}".format(emp_id)
+                            ).json()
 
-    # Get completed todos made by specific employee ( by id)
-    completed_todos = requests.get(todos_url, params={"completed": "true",
-                                                      "userId": emp_id}).json()
+        # Get completed todos made by specific employee ( by id)
+        completed_todos = requests.get(todos_url, params={"completed": "true",
+                                                        "userId": emp_id}).json()
 
-    emp_name = emp.get("name")
-    total_number_of_tasks = len(todos)
-    number_of_done_tasks = len(completed_todos)
+        emp_name = emp.get("name")
+        total_number_of_tasks = len(todos)
+        number_of_done_tasks = len(completed_todos)
 
-    print("Employee {} is done with tasks({}/{}):".format(
-        emp_name,
-        number_of_done_tasks,
-        total_number_of_tasks
-    ))
+        print("Employee {} is done with tasks({}/{}):".format(
+            emp_name,
+            number_of_done_tasks,
+            total_number_of_tasks
+        ))
 
-    for todo in completed_todos:
-        print(f"\t {todo.get('title')}")
+        for todo in completed_todos:
+            print(f"\t {todo.get('title')}")
+    
+    except IndexError:
+        print("Usage: ./0-gather_data_from_an_API.py EMP_ID")
+    except Exception:
+        print("Error while fetching data")
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,26 +6,31 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    emp_id = argv[1]
+    try:
+        emp_id = argv[1]
 
-    # Get employee by ID
-    emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-        emp_id)
-    ).json()
+        # Get employee by ID
+        emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
+            emp_id)
+        ).json()
 
-    # Get all todos by specific employee (by id)
-    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+        # Get all todos by specific employee (by id)
+        todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
-    todos = requests.get(todos_url, params="userId={}".format(emp_id)
-                         ).json()
+        todos = requests.get(todos_url, params="userId={}".format(emp_id)
+                            ).json()
 
-    emp_username = emp.get("username")
+        emp_username = emp.get("username")
 
-    # Export data in the CSV format.
-    with open(f"{emp_id}.csv", "w") as wf:
-        csv_writer = csv.writer(wf)
+        # Export data in the CSV format.
+        with open(f"{emp_id}.csv", "w") as wf:
+            csv_writer = csv.writer(wf, quotechar='"', quoting=csv.QUOTE_ALL)
 
-        for todo in todos:
-            csv_writer.writerow([emp_id, emp_username,
-                                 todo.get("completed"),
-                                 todo.get("title")])
+            for todo in todos:
+                csv_writer.writerow([emp_id, emp_username,
+                                    todo.get("completed"),
+                                    todo.get("title")])
+    except IndexError:
+        print("Usage: ./1-export_to_CSV.py EMP_ID")
+    except Exception:
+        print("Error while fetching data")
