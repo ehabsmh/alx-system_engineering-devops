81c706a165771f137957d6c385be3f077a68352c
Ehab Elsayed <ehabsmh3@gmail.com>
2024-01-29 20:54:28 +0200

    pep8 style added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,19 +9,19 @@ if __name__ == "__main__":
         emp_id = argv[1]
 
         # Get employee by ID
-        emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-            emp_id)
-        ).json()
+        emp_url = "https://jsonplaceholder.typicode.com/users/{}"
+        emp = requests.get(emp_url.format(emp_id)).json()
 
         # Get all todos by specific employee (by id)
         todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
-        todos = requests.get(todos_url, params="userId={}".format(emp_id)
-                            ).json()
+        todos = requests.get(todos_url, params="userId={}".
+                             format(emp_id)).json()
 
         # Get completed todos made by specific employee ( by id)
-        completed_todos = requests.get(todos_url, params={"completed": "true",
-                                                        "userId": emp_id}).json()
+        completed_todos = requests.get(todos_url,
+                                       params={"completed": "true",
+                                               "userId": emp_id}).json()
 
         emp_name = emp.get("name")
         total_number_of_tasks = len(todos)
@@ -35,7 +35,7 @@ if __name__ == "__main__":
 
         for todo in completed_todos:
             print(f"\t {todo.get('title')}")
-    
+
     except IndexError:
         print("Usage: ./0-gather_data_from_an_API.py EMP_ID")
     except Exception:
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,21 +10,22 @@ if __name__ == "__main__":
         emp_id = argv[1]
 
         # Get employee by ID
-        emp = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(
-            emp_id)
-        ).json()
+        emp_url = "https://jsonplaceholder.typicode.com/users/{}"
+        emp = requests.get(emp_url.format(emp_id)).json()
 
         # Get all todos by specific employee (by id)
         todos_url = "https://jsonplaceholder.typicode.com/todos/"
 
-        todos = requests.get(todos_url, params="userId={}".format(emp_id)
-                            ).json()
+        todos = requests.get(todos_url, params="userId={}".
+                             format(emp_id)).json()
 
         emp_username = emp.get("username")
 
         # Export data in the CSV format.
         with open(f"{emp_id}.csv", "w") as wf:
-            csv_writer = csv.writer(wf, quotechar='"', quoting=csv.QUOTE_ALL)
+            csv_writer = csv.writer(wf,
+                                    quotechar='"',
+                                    quoting=csv.QUOTE_ALL)
 
             for todo in todos:
                 csv_writer.writerow([emp_id, emp_username,
