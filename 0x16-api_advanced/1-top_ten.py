942c5a413b230daf646868ea97973bbaea329e8d
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 19:59:45 +0200

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit."""
+"""Queries the Reddit API and prints the titles"""
+import requests
 
 
 def top_ten(subreddit):
-    """function that queries the Reddit API and prints the titles
-    of the first 10 hot posts listed for a given subreddit."""
-    import requests
-    import sys
-
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    prints None if the subreddit is invalid.
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
-        print("None")
+    headers = requests.utils.default_headers()
+    headers.update({'User-Agent': 'My User Agent 1.0'})
+
+    response = requests.get(url, headers=headers, params={"limit": 10}).json()
+    top_ten_posts = response.get('data', {}).get('children', [])
+
+    if not top_ten_posts:
+        print(None)
+        return
+
+    for post in top_ten_posts:
+        print(post.get('data', {}).get('title'))
