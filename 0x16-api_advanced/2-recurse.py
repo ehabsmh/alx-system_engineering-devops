7ca37913a5bb2521830ba18925bc3fc64efd60c6
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 20:20:10 +0200

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 """Recursive function that queries the Reddit API and returns a list"""
 import requests
+import sys
 
 
-def recurse(subreddit, hot_list=[], after="", counter=0):
-    """Returns a list of titles of all hot posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
     headers = {
-        "User-Agent": "My User Agent 1.0"
+        'User-Agent': u_agent
     }
+
     params = {
-        "after": after,
-        "counter": counter,
+        'after': after
     }
-    res = requests.get(url, headers=headers, params=params,
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    res = requests.get(url,
+                       headers=headers,
+                       params=params,
                        allow_redirects=False)
-    if res.status_code == 404:
+
+    if res.status_code != 200:
         return None
 
-    results = res.json().get("data")
-    after = results.get("after")
-    counter += results.get("dist")
-
-    for child in results.get("children"):
-        resulted_child = child.get("data").get("title")
-        hot_list.append(resulted_child)
-
-    if after is not None:
-        return recurse(subreddit, hot_list, after, counter)
-    return hot_list
+    dic = res.json()
+    hot_posts = dic['data']['children']
+    add_title(hot_list, hot_posts)
+    after = dic['data']['after']
+    if not after:
+        return hot_list
+    return recurse(subreddit, hot_list=hot_list, after=after)
