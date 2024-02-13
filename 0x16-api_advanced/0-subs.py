42d9c83fb596efc6f44500031992794107dd2e61
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 19:03:35 +0200

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""
-Query Reddit API for number of subscribers for a given subreddit
-"""
+"""Query Reddit API for number of subscribers for a given subreddit"""
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-        return number of subscribers for a given subreddit
-        return 0 if invalid subreddit given
+    Returns the number of subscribers for a given subreddit.
+    returns 0 if the subreddit is invalid.
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get('data', {}).get('subscribers')
+    response = requests.get(url, headers=headers).json()
+    subscribers = response.get('data', {}).get('subscribers')
     if not subscribers:
         return 0
+
     return subscribers
