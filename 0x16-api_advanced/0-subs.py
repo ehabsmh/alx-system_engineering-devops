a4fb2e1b3c4ea8d10f4dac42a138370516082c50
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 18:52:54 +0200

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'script by Ehab Hussein'}
+    headers = {'User-Agent': 'My User Agent 1.0'}
     response = get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
