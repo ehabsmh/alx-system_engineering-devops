395a7d24a28afe7d838115c574e36089a4e38a78
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 20:24:54 +0200

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,25 +4,21 @@ import requests
 import sys
 
 
-def add_title(hot_list, hot_posts):
+def append_title(hot_list, hot_posts):
     """ Adds item into a list """
-    if len(hot_posts) == 0:
+    if not len(hot_posts):
         return
+
     hot_list.append(hot_posts[0]['data']['title'])
     hot_posts.pop(0)
-    add_title(hot_list, hot_posts)
+    append_title(hot_list, hot_posts)
 
 
 def recurse(subreddit, hot_list=[], after=None):
     """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
-    headers = {
-        'User-Agent': u_agent
-    }
+    headers = {'User-Agent': 'My User Agent 1.0'}
 
-    params = {
-        'after': after
-    }
+    params = {'after': after}
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     res = requests.get(url,
@@ -30,13 +26,15 @@ def recurse(subreddit, hot_list=[], after=None):
                        params=params,
                        allow_redirects=False)
 
-    if res.status_code != 200:
+    if not res:
         return None
 
-    dic = res.json()
-    hot_posts = dic['data']['children']
-    add_title(hot_list, hot_posts)
-    after = dic['data']['after']
+    post = res.json()
+    hot_posts = post['data']['children']
+    append_title(hot_list, hot_posts)
+    after = post['data']['after']
+
     if not after:
         return hot_list
+
     return recurse(subreddit, hot_list=hot_list, after=after)
