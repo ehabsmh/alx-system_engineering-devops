8fda83bf100e588f3d072cda21605bf1bae87c9a
Ehab Elsayed <ehabsmh3@gmail.com>
2024-02-13 18:58:04 +0200

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,12 +4,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
+    """Returns the number of subscribers for a given subreddit.
+    returns 0 if the subreddit is invalid.
+    """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
     response = requests.get(url, headers=headers).json()
-    subscribers = response.get('data').get('subscribers')
+    subscribers = response.get('data', {}).get('subscribers')
     if not subscribers:
         return 0
     return subscribers
